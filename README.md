9a1db16bb3c4445dc15eddb71f6a27b89d1a0c7a
ErwinPrograms <erwinprograms@gmail.com>
2024-02-08 16:11:06 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a discord chat bot with simple dice rolling commands.
+
+Made with a tutorial by Indently [Video Link](https://www.youtube.com/watch?v=UYJDKSah-Ww)
---
+++ responses.py
@@ -0,0 +1,15 @@
+from random import choice, randint
+
+def get_response(user_input: str) -> str:
+    lowered: str = user_input.lower()
+
+    if lowered == '':
+        return 'Well, you\'re awfully silent...'
+    elif 'hello' in lowered:
+        return 'Hello there!'
+    elif 'roll' in lowered:
+        return f'You rolled: {randint(1,6)}'
+    else:
+        return choice(['I do not understand...',
+                       'What are you talking about?',
+                       'Can you rephrase that for me?'])
